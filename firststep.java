d82fb9028a1428af8579085dd13c58d21f157807
Eberhard Hüsch <32211893+dehusch@users.noreply.github.com>
2021-08-20 22:52:18 -0300

    Last uptade

--- firststep.java
+++ firststep.java
@@ -1,3 +1,11 @@
+//Para compilar em Java primeiro deve exercutar
+//o comando: javac nomedoarquivo.java
+//Depois de compilado, digitar:
+// java nomedoarquivo
+/*
+    Comentarios podem ser adicionado iniciando com /* e
+    finalizando com 
+*/
 public class firststep{
     public static void main(String[] args) {
         System.out.println("Ola, Tudo bem?");
